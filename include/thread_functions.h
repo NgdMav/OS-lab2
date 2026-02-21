7931e7e304bb4828b9ab08d626ef59f70271c894
NgdMav <riherr00@gmail.com>
2026-02-21 15:52:57 +0300

    struct to send min-max data

---
+++ include/thread_functions.h
@@ -0,0 +1,13 @@
+#ifndef THREAD_FUNCTIONS_H
+#define THREAD_FUNCTIONS_H
+
+struct MinMaxData {
+    int* array;
+    int size;
+    int minValue;
+    int maxValue;
+    int minIndex;
+    int maxIndex;
+};
+
+#endif // THREAD_FUNCTIONS_H
